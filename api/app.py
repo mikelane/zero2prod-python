c66c9e72b1becd25380b5f09ca819f2aee10451b
Mike Lane <mikelane@gmail.com>
2023-02-06 16:02:40 -0800

    Move the health check endpoint to /health

--- api/app.py
+++ api/app.py
@@ -4,7 +4,7 @@ from starlite import (
 )
 
 
-@get('/')
+@get('/health')
 async def health_check() -> dict[str, str]:
     return {'status': 'ok'}
 
--- tests/test_api.py
+++ tests/test_api.py
@@ -19,6 +19,6 @@ def client(app) -> AsyncTestClient:
 
 @pytest.mark.asyncio()
 async def it_returns_a_response_to_a_health_check(client):
-    response = await client.get('/')
+    response = await client.get('/health')
     assert response.status_code == 200
     assert response.json() == {'status': 'ok'}
