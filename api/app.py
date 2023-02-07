41baf17815e2524e931055943781168bc0a38b2f
Mike Lane <mikelane@gmail.com>
2023-02-06 16:13:18 -0800

    Change health check return type to be a pydantic model

--- api/app.py
+++ api/app.py
@@ -1,12 +1,21 @@
+from typing import NewType
+
+from pydantic import BaseModel
 from starlite import (
     Starlite,
     get,
 )
 
+StatusMessage = NewType('StatusMessage', str)
+
+
+class HealthCheckResponse(BaseModel):
+    status: StatusMessage
+
 
 @get('/health')
-async def health_check() -> dict[str, str]:
-    return {'status': 'ok'}
+async def health_check() -> HealthCheckResponse:
+    return HealthCheckResponse(status='ok')
 
 
 app = Starlite(route_handlers=[health_check])
